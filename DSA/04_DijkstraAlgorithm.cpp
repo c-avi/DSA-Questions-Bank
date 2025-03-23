3f3e8314e2f63a28ed3f2261c75b1520a458c191
Chhavi <gusainchhavi@gmail.com>
2025-03-23 11:48:29 +0530

    add file

---
+++ DSA/04_DijkstraAlgorithm.cpp
@@ -0,0 +1,87 @@
+// Program to find Dijkstra's shortest path using
+// priority_queue
+#include <bits/stdc++.h>
+using namespace std;
+
+// To add an edge
+void addEdge(vector<pair<int, int> > adj[], int u, int v,
+             int wt)
+{
+    adj[u].push_back(make_pair(v, wt));
+    adj[v].push_back(make_pair(u, wt));
+}
+
+// Prints shortest paths from src to all other vertices
+void shortestPath(vector<pair<int, int> > adj[], int V,
+                  int src)
+{
+    // Create a priority queue to store vertices that
+    // are being preprocessed.
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> >
+        pq;
+
+    // Create a vector for distances and initialize all
+    // distances as infinite
+    vector<int> dist(V, INT_MAX);
+
+    // Insert source itself in priority queue and initialize
+    // its distance as 0.
+    pq.push(make_pair(0, src));
+    dist[src] = 0;
+
+    /* Looping till priority queue becomes empty (or all
+    distances are not finalized) */
+    while (!pq.empty()) {
+        // The first vertex in pair is the minimum distance
+        // vertex, extract it from priority queue.
+        int u = pq.top().second;
+        pq.pop();
+
+        // Get all adjacent of u.
+        for (auto x : adj[u]) {
+            // Get vertex label and weight of current
+            // adjacent of u.
+            int v = x.first;
+            int weight = x.second;
+
+            // If there is shorted path to v through u.
+            if (dist[v] > dist[u] + weight) {
+                // Updating distance of v
+                dist[v] = dist[u] + weight;
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+
+    // Print shortest distances stored in dist[]
+    printf("Vertex \tDistance from Source\n");
+    for (int i = 0; i < V; ++i)
+        printf("%d \t\t %d\n", i, dist[i]);
+}
+
+// Driver program to test methods of graph class
+int main()
+{
+    int V = 9;
+    vector<pair<int,int>> adj[V];
+
+    // making above shown graph
+    addEdge(adj, 0, 1, 4);
+    addEdge(adj, 0, 7, 8);
+    addEdge(adj, 1, 2, 8);
+    addEdge(adj, 1, 7, 11);
+    addEdge(adj, 2, 3, 7);
+    addEdge(adj, 2, 8, 2);
+    addEdge(adj, 2, 5, 4);
+    addEdge(adj, 3, 4, 9);
+    addEdge(adj, 3, 5, 14);
+    addEdge(adj, 4, 5, 10);
+    addEdge(adj, 5, 6, 2);
+    addEdge(adj, 6, 7, 1);
+    addEdge(adj, 6, 8, 6);
+    addEdge(adj, 7, 8, 7);
+
+    shortestPath(adj, V, 0);
+
+    return 0;
+}
