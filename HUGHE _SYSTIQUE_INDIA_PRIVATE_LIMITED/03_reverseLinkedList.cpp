21a38150ec1fce789cb022264a3e516847968349
Chhavi <gusainchhavi@gmail.com>
2025-08-18 21:51:45 +0530

    add file

---
+++ HUGHE _SYSTIQUE_INDIA_PRIVATE_LIMITED/03_reverseLinkedList.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = NULL;
+    ListNode* curr = head;
+    ListNode* nextNode = NULL;
+
+    while (curr != NULL) {
+        nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+    return prev;
+}
+
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val<< " ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = reverseList(head);
+    cout << "Reversed List: ";
+    printList(head);
+
+    return 0;
+}
